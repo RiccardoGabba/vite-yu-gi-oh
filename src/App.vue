1b1e5d323f0f1ca8b29069cdde7ef4deaba1d229
filippo <riccardo.gabba02@gmail.com>
2023-11-14 11:29:05 +0100

    imported the cards into the main app

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,27 @@
     <div class="container">
       <div class="row">
         <!--Aggiungere le cards qui-->
-
+        <Cards />
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import Cards from './components/Cards.vue'
   export default {
-    
+    name: 'App',
+    components: {
+      Cards,
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
   }
 </script>
 
