09f638785c9a816a65a37dc429afced996acc875
filippo <riccardo.gabba02@gmail.com>
2023-11-14 11:39:00 +0100

    imported the store in the app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <div class="container">
       <div class="row">
         <!--Aggiungere le cards qui-->
-        <Cards />
+        <Cards
+          v-for="(el) in store.cardList"
+          :name="el.name"
+          :archetype="el.archetype"
+          :image="el.card_images[0].image_url"
+        />
       </div>
     </div>
   </main>
@@ -11,6 +16,9 @@
 
 <script>
 import Cards from './components/Cards.vue'
+import { store } from './data/store'
+import axios from 'axios'
+
   export default {
     name: 'App',
     components: {
@@ -18,15 +26,19 @@ import Cards from './components/Cards.vue'
     },
     data() {
       return {
-
+        store,
       }
     },
     methods: {
-
+      getCards(){
+        const url = store.apiUrl
+        axios.get(url).then(response => {
+          console.log(response.data.data)
+          this.store.cardList = response.data.data
+        }
+      )}
     },
   }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
